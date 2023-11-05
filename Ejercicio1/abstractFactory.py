48c08f867774c0677d901abbf8ecd323f766efd5
rnoguer22 <rnogugon@myuax.com>
2023-11-05 19:53:28 +0100

    Hay que darle una vuelta pero debera ser algo asi

--- Ejercicio1/abstractFactory.py
+++ Ejercicio1/abstractFactory.py
@@ -12,11 +12,11 @@ class AbstractFactory(ABC):
     another.
     """
     @abstractmethod
-    def create_product_a(self) -> AbstractProductA:
+    def create_graphic(self) -> AbstractGraphics:
         pass
 
     @abstractmethod
-    def create_product_b(self) -> AbstractProductB:
+    def calculate_statistic(self) -> AbstractStatistics:
         pass
 
 
@@ -28,33 +28,92 @@ class ConcreteFactory1(AbstractFactory):
     product, while inside the method a concrete product is instantiated.
     """
 
-    def create_product_a(self) -> AbstractProductA:
-        return ConcreteProductA1()
+    def create_graphic(self) -> AbstractGraphics:
+        return Mean()
 
-    def create_product_b(self) -> AbstractProductB:
-        return ConcreteProductB1()
+    def calculate_statistic(self) -> AbstractStatistics:
+        return GraficoBarras()
 
 
 class ConcreteFactory2(AbstractFactory):
     """
-    Each Concrete Factory has a corresponding product variant.
+    Concrete Factories produce a family of products that belong to a single
+    variant. The factory guarantees that resulting products are compatible. Note
+    that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
     """
 
-    def create_product_a(self) -> AbstractProductA:
-        return ConcreteProductA2()
+    def create_graphic(self) -> AbstractGraphics:
+        return Mode()
 
-    def create_product_b(self) -> AbstractProductB:
-        return ConcreteProductB2()
+    def calculate_statistic(self) -> AbstractStatistics:
+        return GraficoBarras()
+    
+class ConcreteFactory3(AbstractFactory):
+    """
+    Concrete Factories produce a family of products that belong to a single
+    variant. The factory guarantees that resulting products are compatible. Note
+    that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
+    """
+
+    def create_graphic(self) -> AbstractGraphics:
+        return Median()
+
+    def calculate_statistic(self) -> AbstractStatistics:
+        return GraficoBarras()
+    
+class ConcreteFactory4(AbstractFactory):
+    """
+    Concrete Factories produce a family of products that belong to a single
+    variant. The factory guarantees that resulting products are compatible. Note
+    that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
+    """
+
+    def create_graphic(self) -> AbstractGraphics:
+        return Mean()
+
+    def calculate_statistic(self) -> AbstractStatistics:
+        return GraficoHistograma()
+    
+class ConcreteFactory5(AbstractFactory):
+    """
+    Concrete Factories produce a family of products that belong to a single
+    variant. The factory guarantees that resulting products are compatible. Note
+    that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
+    """
+
+    def create_graphic(self) -> AbstractGraphics:
+        return Mode()
+
+    def calculate_statistic(self) -> AbstractStatistics:
+        return GraficoHistograma()
+    
+class ConcreteFactory6(AbstractFactory):
+    """
+    Concrete Factories produce a family of products that belong to a single
+    variant. The factory guarantees that resulting products are compatible. Note
+    that signatures of the Concrete Factory's methods return an abstract
+    product, while inside the method a concrete product is instantiated.
+    """
+
+    def create_graphic(self) -> AbstractGraphics:
+        return Median()
+
+    def calculate_statistic(self) -> AbstractStatistics:
+        return GraficoHistograma()
 
 
-class AbstractProductA(ABC):
+class AbstractGraphics(ABC):
     """
     Each distinct product of a product family should have a base interface. All
     variants of the product must implement this interface.
     """
 
     @abstractmethod
-    def useful_function_a(self) -> str:
+    def create_graphic(self) -> str:
         pass
 
 
@@ -63,31 +122,31 @@ Concrete Products are created by corresponding Concrete Factories.
 """
 
 
-class ConcreteProductA1(AbstractProductA):
-    def useful_function_a(self) -> str:
-        return "The result of the product A1."
+class GraficoBarras(AbstractGraphics):
+    def create_graphic(self) -> str:
+        return "Devolveria un grafico de barras"
 
 
-class ConcreteProductA2(AbstractProductA):
-    def useful_function_a(self) -> str:
-        return "The result of the product A2."
+class GraficoHistograma(AbstractGraphics):
+    def create_graphic(self) -> str:
+        return "Devolveria un histograma"
 
 
-class AbstractProductB(ABC):
+class AbstractStatistics(ABC):
     """
     Here's the the base interface of another product. All products can interact
     with each other, but proper interaction is possible only between products of
     the same concrete variant.
     """
     @abstractmethod
-    def useful_function_b(self) -> None:
+    def calculate_statistic(self) -> None:
         """
         Product B is able to do its own thing...
         """
         pass
 
     @abstractmethod
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
+    def graphic_statistic(self, collaborator: AbstractGraphics) -> None:
         """
         ...but it also can collaborate with the ProductA.
 
@@ -102,9 +161,9 @@ Concrete Products are created by corresponding Concrete Factories.
 """
 
 
-class ConcreteProductB1(AbstractProductB):
-    def useful_function_b(self) -> str:
-        return "The result of the product B1."
+class Mean(AbstractStatistics):
+    def calculate_statistic(self) -> str:
+        return "Devolveria la media"
 
     """
     The variant, Product B1, is only able to work correctly with the variant,
@@ -112,23 +171,36 @@ class ConcreteProductB1(AbstractProductB):
     argument.
     """
 
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
-        result = collaborator.useful_function_a()
-        return f"The result of the B1 collaborating with the ({result})"
+    def graphic(self, collaborator: AbstractGraphics) -> str:
+        result = collaborator.create_graphic()
+        return "Devolveria un grafico de la media"
 
 
-class ConcreteProductB2(AbstractProductB):
-    def useful_function_b(self) -> str:
-        return "The result of the product B2."
+class Mode(AbstractStatistics):
+    def calculate_statistic(self) -> str:
+        return "Calcularia la moda"
 
-    def another_useful_function_b(self, collaborator: AbstractProductA):
+    def graphic(self, collaborator: AbstractGraphics) -> str:
         """
         The variant, Product B2, is only able to work correctly with the
         variant, Product A2. Nevertheless, it accepts any instance of
         AbstractProductA as an argument.
         """
-        result = collaborator.useful_function_a()
-        return f"The result of the B2 collaborating with the ({result})"
+        result = collaborator.create_graphic()
+        return "Devolveria un grafico de la moda"
+    
+class Median(AbstractStatistics):
+    def calculate_statistic(self) -> str:
+        return "Calcularia la mediana"
+
+    def graphic(self, collaborator: AbstractGraphics) -> str:
+        """
+        The variant, Product B2, is only able to work correctly with the
+        variant, Product A2. Nevertheless, it accepts any instance of
+        AbstractProductA as an argument.
+        """
+        result = collaborator.create_graphic()
+        return "Devolveria un grafico de la mediana"
 
 
 def client_code(factory: AbstractFactory) -> None:
