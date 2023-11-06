833dd14a34288189b976dc61619889d9b028a32d
rnoguer22 <rnogugon@myuax.com>
2023-11-06 12:19:40 +0100

    Graficos (mas o meno)

--- Ejercicio1/abstractFactory.py
+++ Ejercicio1/abstractFactory.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
@@ -74,13 +75,21 @@ class AbstractGraphics(ABC):
 
 class GraficoBarras(AbstractGraphics):
     def create_graphic(self) -> str:
-        return "Devolveria un grafico de barras"
+        # Crear el diagrama de barras
+        plt.bar(range(50), diferencia[:50], color='b', alpha=0.5)
+        plt.title('Diagrama de Barras de Diferencias de Tiempo')
+        return "Barplot"
 
 
 class GraficoHistograma(AbstractGraphics):
     def create_graphic(self) -> str:
-        return "Devolveria un histograma"
-
+        # Crear el histograma
+        plt.hist([df['Hora Intervención'], df['Hora Solicitud']], bins=25)
+        # Configuración adicional
+        plt.xlabel('Diferencia de Tiempo (segundos)')
+        plt.ylabel('Frecuencia')
+        plt.title('Histograma de Diferencias de Tiempo')
+        return 'histograma'
 
 class AbstractStatistics(ABC):
     @abstractmethod
@@ -98,8 +107,9 @@ class Mean(AbstractStatistics):
         return round(diferencia.mean().seconds/60, 2)
 
     def graphic(self, collaborator: AbstractGraphics) -> str:
-        result = collaborator.create_graphic()
-        return "Devolveria un grafico de la media"
+        grafico = collaborator.create_graphic()
+        plt.axhline(y=self.calculate_statistic(), color='r', linestyle='--', label='Mean')
+        plt.savefig('Ejercicio1/Graficos/media_{}.png'.format(grafico))
 
 
 class Mode(AbstractStatistics):
@@ -108,8 +118,9 @@ class Mode(AbstractStatistics):
         return round(diferencia.mode()[0].seconds/60, 2)
 
     def graphic(self, collaborator: AbstractGraphics) -> str:
-        result = collaborator.create_graphic()
-        return "Devolveria un grafico de la moda"
+        grafico = collaborator.create_graphic()
+        plt.axhline(y=self.calculate_statistic(), color='r', linestyle='--', label='Mode')
+        plt.savefig('Ejercicio1/Graficos/moda_{}.png'.format(grafico))
     
 class Median(AbstractStatistics):
     def calculate_statistic(self) -> str:
@@ -117,8 +128,9 @@ class Median(AbstractStatistics):
         return round(diferencia.median().seconds/60, 2)
 
     def graphic(self, collaborator: AbstractGraphics) -> str:
-        result = collaborator.create_graphic()
-        return "Devolveria un grafico de la mediana"
+        grafico = collaborator.create_graphic()
+        plt.axhline(y=self.calculate_statistic(), color='r', linestyle='--', label='Median')
+        plt.savefig('Ejercicio1/Graficos/mediana_{}.png'.format(grafico))
 
 
 def client_code(factory: AbstractFactory) -> None:
@@ -126,7 +138,6 @@ def client_code(factory: AbstractFactory) -> None:
     statistic = factory.calculate_statistic()
 
     print(f"{statistic.calculate_statistic()}")
-    print(f"{statistic.graphic(graphic)}")
     print(f"{graphic.create_graphic()}")
 
 if __name__ == "__main__":
@@ -137,8 +148,15 @@ if __name__ == "__main__":
     #Calcula la diferencia entre la hora de solicitud y la hora de intervencion, ya que es lo unico que tiene sentido para una media
     diferencia = df['Hora Intervención'] - df['Hora Solicitud']
 
+
     client_code(ConcreteFactory1())
     print("\n")
     client_code(ConcreteFactory2())        
     print("\n")
     client_code(ConcreteFactory3())    
+    print("\n")
+    client_code(ConcreteFactory4())    
+    print("\n")
+    client_code(ConcreteFactory5())    
+    print("\n")
+    client_code(ConcreteFactory6())   
